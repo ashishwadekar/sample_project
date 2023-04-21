826a0d4c699e8d294aaee1aa90764474130f5070
Anuj Shukla <anujshukla151296@gmail.com>
2023-04-21 16:23:16 +0530

    Generated Credit Card  Model
    add field in Credit card model is card_holder name & card number

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_21_102204) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_21_104707) do
   create_table "bank_account_transactions", force: :cascade do |t|
     t.string "account_number"
     t.string "ifsc_code"
@@ -21,6 +21,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_21_102204) do
   create_table "bank_accounts", force: :cascade do |t|
     t.string "account_number", null: false
     t.string "ifsc_code", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "credit_cards", force: :cascade do |t|
+    t.string "card_holder_name"
+    t.string "card_number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
