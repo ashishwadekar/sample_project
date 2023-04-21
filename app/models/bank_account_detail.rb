70995bc38af1d9118f975d6201c2de4e41e074e0
jitendra kakade <jitendrakakade03@gmail.com>
2023-04-21 16:37:47 +0530

    validation to Bank_account_Details & database User Bank Account Details Validation Done User Cant Enter Empty value User Show Message  & one more valiation db file user cant enter null value

--- app/models/bank_account_detail.rb
+++ app/models/bank_account_detail.rb
@@ -1,2 +1,6 @@
 class BankAccountDetail < ApplicationRecord
+  validates :account_no, presence: true
+  validates :ifsc_code, presence: true
+  validates :bank_name, presence: true
+  validates :user_name, presence: true
 end
--- db/migrate/20230421104342_create_bank_account_details.rb
+++ db/migrate/20230421104342_create_bank_account_details.rb
@@ -1,10 +1,10 @@
 class CreateBankAccountDetails < ActiveRecord::Migration[7.0]
   def change
     create_table :bank_account_details do |t|
-      t.string :account_no
-      t.string :ifsc_code
-      t.string :bank_name
-      t.string :user_name
+      t.string :account_no, null: false
+      t.string :ifsc_code, null: false
+      t.string :bank_name, null: false
+      t.string :user_name, null: false
 
       t.timestamps
     end
